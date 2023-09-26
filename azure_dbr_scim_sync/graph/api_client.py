c740237451e766978606cdcd1e84adc4e74c1ca9
Grzegorz Rusin <grzegorz.rusin@databricks.com>
2023-09-26 12:35:12 +0200

    cleaner

--- azure_dbr_scim_sync/graph/api_client.py
+++ azure_dbr_scim_sync/graph/api_client.py
@@ -1,6 +1,3 @@
-import logging
-import os
-import sys
 
 import requests
 
@@ -52,56 +49,3 @@ class GraphAPIClient:
         res.raise_for_status()
 
         return res.json().get("value")
-
-    def _find_app(self):
-        res = requests.get(
-            f"{self._base_url}/servicePrincipals?$filter=startswith(displayName,'{self._app_name}')&$count=true&$top=1",
-            headers=self._header)
-        res.raise_for_status()
-
-        value = res.json().get("value")
-
-        if len(value) == 0:
-            raise ValueError(f"Failed to find app: {self._app_name}")
-
-        group = value[0]
-
-        return {
-            "appId": group.get("appId"),
-            "roleId": group.get("appRoles")[0].get("id"),
-            "objectId": group.get("id")
-        }
-
-
-if __name__ == "__main__":
-    # arg_parser = argparse.ArgumentParser()
-    # arg_parser.add_argument("--tenant_id", help="Azure Tenant Id", required=True)
-    # arg_parser.add_argument("--spn_id", help="Deployment SPN Id", required=False)
-    # arg_parser.add_argument("--spn_key", help="Deployment SPN Secret Key", required=False)
-    # arg_parser.add_argument("--groups_json_file_name", help="JSON file containing all groups", required=True)
-    # arg_parser.add_argument("--verbose",
-    #                         help="Verbose logs",
-    #                         default=False,
-    #                         required=False,
-    #                         action='store_true')
-    # args = vars(arg_parser.parse_args())
-
-    tenant_id = os.getenv('ARM_TENANT_ID')
-    spn_id = os.getenv('ARM_CLIENT_ID')
-    spn_key = os.getenv('ARM_CLIENT_SECRET')
-
-    logging.basicConfig(stream=sys.stderr,
-                        level=(logging.DEBUG),
-                        format='%(asctime)s %(levelname)s %(threadName)s [%(name)s] %(message)s')
-
-    logger = logging.getLogger('sync')
-
-    logger.info(f"creds {tenant_id=}, {spn_id=}")
-
-    ec = GraphAPIClient(tenant_id=tenant_id, spn_id=spn_id, spn_key=spn_key)
-
-    group_name = 'team02-admin'
-    group_info = ec.get_group_by_name(group_name)
-
-    logger.info(f"group by id: {ec.get_group_by_id('bd6f0f0c-e528-47f8-b67e-a3d299852083')}")
-    logger.info(f"group by name: {ec.get_group_by_name('team02-admin')}")
