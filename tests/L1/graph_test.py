cd9d39b0b4adef95e6dbcd5a6d358b821a4e8b60
Grzegorz Rusin <grzegorz.rusin@databricks.com>
2023-09-27 15:42:13 +0200

    intergration tests

--- tests/L1/graph_test.py
+++ tests/L1/graph_test.py
@@ -48,10 +48,3 @@ def test_non_existing_group(client: GraphAPIClient):
     assert group_info is None
 
 
-def test_graph_sync_object(client: GraphAPIClient):
-    stuff_to_sync = client.get_objects_for_sync([
-        'team02-admin', 'team01-admin', 'team02-eng', 'team01-eng', 'uc-metastore-playground-admin',
-        'uc-account-admin'
-    ])
-
-    assert stuff_to_sync
---
+++ tests/L2/graph_to_scim_test.py
@@ -0,0 +1,54 @@
+
+import json
+import logging
+import os
+import sys
+
+import pytest
+
+from azure_dbr_scim_sync.graph import GraphAPIClient
+from databricks.sdk import AccountClient
+
+from azure_dbr_scim_sync.scim import (create_or_update_groups,
+                                      create_or_update_service_principals,
+                                      create_or_update_users)
+
+logging.basicConfig(stream=sys.stderr,
+                    level=(logging.DEBUG),
+                    format='%(asctime)s %(levelname)s %(threadName)s [%(name)s] %(message)s')
+
+logger = logging.getLogger('sync')
+
+
+@pytest.fixture()
+def graph_client():
+    tenant_id = os.getenv('ARM_TENANT_ID')
+    assert tenant_id
+
+    spn_id = os.getenv('ARM_CLIENT_ID')
+    assert spn_id
+
+    spn_key = os.getenv('ARM_CLIENT_SECRET')
+    assert spn_key
+
+    return GraphAPIClient(tenant_id=tenant_id, spn_id=spn_id, spn_key=spn_key)
+
+@pytest.fixture()
+def account_client():
+    account_id = os.getenv("DATABRICKS_ACCOUNT_ID")
+    assert account_id
+
+    host = os.getenv("DATABRICKS_HOST")
+    assert host
+
+    return AccountClient(host=host, account_id=account_id)
+
+def test_graph_sync_object(graph_client: GraphAPIClient, account_client: AccountClient):
+    stuff_to_sync = graph_client.get_objects_for_sync([
+        'team02-admin', 'team01-admin', 'team02-eng', 'team01-eng', 'uc-metastore-playground-admin',
+        'uc-account-admin'
+    ])
+    
+    create_or_update_users(account_client, [x.to_sdk_user() for x in stuff_to_sync.users.values()])
+    create_or_update_groups(account_client, [x.to_sdk_group() for x in stuff_to_sync.groups.values()])
+    create_or_update_service_principals(account_client, [x.to_sdk_service_principal() for x in stuff_to_sync.service_principals.values()])
